c88a6eba19acbfd69ef6abc43dbd5b2d6b980f2c
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-11-13 12:29:47 +0530

    minor refactor: few comments

--- firebase/firebaseConfig.py
+++ firebase/firebaseConfig.py
@@ -2,8 +2,9 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 
-
-cred = credentials.Certificate("chess-d400d-firebase-adminsdk-j6xcq-00e9c7244a.json")
+# Make an app in firebase > add realtime database > service accounts > generate new private key
+# Add the json file to the same directory as this file and rename it to "firebaseconfig.json"
+cred = credentials.Certificate("firebaseconfig.json")
 firebase_admin.initialize_app(cred, {
     "databaseURL": "https://chess-13669-default-rtdb.asia-southeast1.firebasedatabase.app/"
 })

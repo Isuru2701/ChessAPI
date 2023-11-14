0471002657f7ad07099f31a7f2b5240238e95f16
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-11-14 08:23:50 +0530

    adding Game support

--- Game.py
+++ Game.py
@@ -1,8 +1,16 @@
 """
 Object for handling the game
 """
+import chess
 
 class Game:
 
     def __init__(self, elo):
         self.__elo = elo
+        self.__stockfish = r"stockfish/stockfish-windows-x86-64-avx2.exe"
+        self.__playerMoves = []
+        self.__aiMoves = []
+        self.__board = chess.Board()
+        engine = chess.engine.SimpleEngine.popen_uci(r"stockfish\stockfish-windows-x86-64-avx2.exe")
+
+
--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import chess
 from flask import Flask, request
+import json
 
+#custom libraries
 from Game import Game
 from firebaseConfig.firebaseConfig import Database
-import json
+
+
 
 app = Flask(__name__)
 
@@ -12,7 +15,11 @@ db = Database()
 @app.route('/api/')
 def start():  # put application's code here
     elo = request.args.get('elo')
+    if elo is None or not elo.isnumeric():
+        elo = 1200 ##default elo
+
     id, token = db.initialize(elo)
+    game = Game(elo)
 
     return json.dumps(
         {

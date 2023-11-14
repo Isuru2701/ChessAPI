517420a7cea513148470c49b02f1cf5bbcf67470
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-11-14 08:48:22 +0530

    minor refactoring - docs

--- Game.py
+++ Game.py
@@ -2,15 +2,32 @@
 Object for handling the game
 """
 import chess
+import chess.engine
 
 class Game:
-
+    """
+    Represents the game
+    Uses stockfish engine
+    """
     def __init__(self, elo):
         self.__elo = elo
         self.__stockfish = r"stockfish/stockfish-windows-x86-64-avx2.exe"
         self.__playerMoves = []
         self.__aiMoves = []
         self.__board = chess.Board()
-        engine = chess.engine.SimpleEngine.popen_uci(r"stockfish\stockfish-windows-x86-64-avx2.exe")
+        self.__engine = chess.engine.SimpleEngine.popen_uci(r"stockfish\stockfish-windows-x86-64-avx2.exe")
+
+    def getMoves(self):
+        """
+        Get the moves of the game
+        :return: player moves and ai moves as arrays
+        """
+        return self.__playerMoves, self.__aiMoves
+
+
+    def destroy(self):
+        """
+        kills the engine
+        """
 
 
